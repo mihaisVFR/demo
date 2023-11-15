d40b268f4a516eaf2ce74c7bb05f420d35e8a258
mihaisVFR <mihbox@bk.ru>
2023-11-15 16:15:02 +0300

    6 tabs done

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import time
-
 import TKinterModernThemes as tkm
 import qrcode
 import tkinter
@@ -37,8 +36,10 @@ class App(tkm.ThemedTKinterFrame):
         self.account = ""
         self.int_receipt_number = 1
         self.receipt_number = f"Чек № {self.int_receipt_number}"
+        self.denom_dict = {"5": 0, "10": 0, "50": 0, "100": 0, "500": 0, "1000": 0, "2000": 0, "5000": 0}
         self.printer_name = win32print.GetDefaultPrinter() # "KPOS_58 Printer"
         self.edit_var = tkinter.StringVar()
+        self.adres = "АДМ №213445 121096 г.Москва\nул.Кастанаевская, д.24 \nEMAIL: sales@deep2000.ru\n"
 
         self.label = None
         self.screen_pad = self.root.winfo_screenwidth() * 0.05
@@ -52,36 +53,31 @@ class App(tkm.ThemedTKinterFrame):
         self.style.map('Treeview', background=[('selected', '#57c8ff')], foreground=[('selected', 'black')])
         self.style.configure('TEntry', font=("Arial", int(self.screen_pad * 0.3)), foreground="white")
         self.style.layout('TNotebook.Tab', [])  # disable tabs layout
-        #self.style.configure('TLabel',  font=("Arial", int(self.screen_pad*0.9), "bold"))  # ridge , groove, sunken, raised
 
         # Notes
         self.notebook = self.Notebook("Notebook Test")
-        self.tab1 = self.notebook.addTab("Авторизация")  # tab1 is a widget frame
-        self.tab2 = self.notebook.addTab("Инкассация")  # tab1 is a widget frame
+        self.tab1 = self.notebook.addTab("Авторизация")
+        self.tab2 = self.notebook.addTab("Инкассация")
         self.tab3 = self.notebook.addTab("Внесение")
         self.tab4 = self.notebook.addTab("Выбор счета")
         self.tab5 = self.notebook.addTab("Чек")
+        self.tab6 = self.notebook.addTab("Закр. опер. день")
+        # self.tab7 = self.notebook.addTab("Откр. опер. день")
+        # self.tab8 = self.notebook.addTab("Ошибка опер. день")
 
         # Tab1
         self.frame1 = self.tab1.addFrame("-")
 
-        # self.frame1.Label(text="", col=0)
-       # label = self.frame1.Label(text="Авторизация", row=0, col=1, colspan=3)
-        #label.configure(font=("Arial", int(self.screen_pad*0.5)), foreground="white")
-        # self.frame1.Label(text="", row=7)
-        # self.frame1.Label(text="", col=4)
-
         self.userinputvar = tkinter.StringVar(value="ЛОГИН")
-
         self.passinputvar = tkinter.StringVar(value="ПАРОЛЬ")
 
         self.user_field = self.frame1.Entry(textvariable=self.userinputvar, col=0, row=0, colspan=3)
         self.user_field.bind("<1>", lambda event: self.clear_user_entry())
         self.password_field = self.frame1.Entry(textvariable=self.passinputvar, col=0, row=1, colspan=3)
         self.password_field.bind("<1>", lambda event: self.clear_password_entry())
-
         self.user_field.configure(font=("Arial", int(self.screen_pad * 0.7)))
         self.password_field.configure(font=("Arial", int(self.screen_pad * 0.7)))
+
         self.button1 = self.frame1.Button("1", lambda: self.digit_buttons(self.button1), col=0, row=2)
         self.button2 = self.frame1.Button("2", lambda: self.digit_buttons(self.button2), col=1, row=2)
         self.button3 = self.frame1.Button("3", lambda: self.digit_buttons(self.button3), col=2, row=2)
@@ -92,34 +88,29 @@ class App(tkm.ThemedTKinterFrame):
         self.button8 = self.frame1.Button("8", lambda: self.digit_buttons(self.button8), col=1, row=4)
         self.button9 = self.frame1.Button("9", lambda: self.digit_buttons(self.button9), col=2, row=4)
         self.button0 = self.frame1.Button("   0   ", lambda: self.digit_buttons(self.button0), col=1, row=5)
+
         self.frame1.AccentButton("УДАЛ", self.backspace, col=0, row=5)
         self.frame1.AccentButton("ВВОД", self.authorization, col=2, row=5)
 
         # Tab2
         frame2 = self.tab2.addFrame("Инкассация")
-        frame2.AccentButton("Открытие\nоперационного дня", lambda: nt[0].select(1),
+        frame2.AccentButton("Открытие\nоперационного дня", lambda: nt[0].select(5),
                                           col=0, row=0, colspan=4, padx=self.screen_pad/2, pady=self.screen_pad/2)
-        frame2.Button("Закрытие\nоперационного дня", lambda: nt[0].select(1),
+        frame2.Button("Закрытие\nоперационного дня", self.close_day,
                                       col=0, row=1, colspan=4, padx=self.screen_pad/2, pady=self.screen_pad/2)
         frame2.Button('❮', lambda: nt[0].select(0), col=4, rowspan=2,  style='x.TButton')
 
         # Tab3
         frame3 = self.tab3.addFrame("Внесение")
-        # frame3.Label(text="", col=0, padx=self.screen_pad)
-        # frame3.Label(text="", row=0, col=1)
-        # frame3.Label(text="", row=4)
         deposit_frame = frame3.addLabelFrame("", col=0, row=1, colspan=5)
         button_frame = frame3.addFrame(name="", col=5, row=1)
 
         label_deposit = deposit_frame.Label(text="99999999")
         label_deposit.configure(font=("Arial", int(self.screen_pad), "bold"), width=9)
-        label_down = frame3.Label("Пожалуйста внесите купюры,\nмаксимальное колличество\n200 купюр.", col=0, row=0, colspan=6)
+        label_down = frame3.Label("Внесите банкноты.\nМаксимальное колличество:\n200 банкнот", col=0, row=0, colspan=6)
         label_down.configure(font=("Arial", int(self.screen_pad*0.8)), foreground="#57c8ff", justify="center")
-        # label_up = frame3.Label("Пожалуйста внесите купюры", col=1, row=1, colspan=4)
-        # label_up.configure(foreground="#57c8ff")
-
         self.done = button_frame.Button('Зачислить', self.receipt)
-        frame3.Button('❮', lambda: nt[0].select(0), col=6, rowspan=2, style='x.TButton')
+        frame3.Button('❮', lambda: nt[0].select(3), col=6, rowspan=2, style='x.TButton')
 
         # Tab4
         self.frame4 = self.tab4.addFrame("Выбор счета")
@@ -149,6 +140,18 @@ class App(tkm.ThemedTKinterFrame):
 
         self.frame5.Button('❮', lambda: nt[0].select(0), col=6, style='x.TButton')
 
+        # Tab6
+        self.denom_dict = {"5": 17, "10": 2, "50": 54, "100": 92, "500": 0, "1000": 1, "2000": 0, "5000": 12}
+        self.frame6 = self.tab6.addFrame("Операционный день закрыт")
+        self.label6 = self.frame6.Label("Операционный день закрыт", size=int(self.screen_pad * 0.8), col=0, row=0, colspan=4)
+        self.label6.configure(foreground="#57c8ff")
+        self.frame6.Seperator(col=0, row=2, colspan=4)
+        self.denom_text = tkinter.Text(self.frame6.master, font=("Arial", int(self.screen_pad*0.3)),
+                                       height=self.screen_pad*0.2, border=False)
+        self.denom_text.tag_configure("center", justify='center')
+        self.denom_text.grid(column=0, row=3, columnspan=4, sticky=N)
+        self.frame6.Button('❮', lambda: nt[0].select(0), col=4, rowspan=4, style='x.TButton')
+
 
 
         #self.bool = tkinter.BooleanVar()
@@ -156,6 +159,18 @@ class App(tkm.ThemedTKinterFrame):
         #self.frame.SlideSwitch("Switch", self.bool)
         #self.togglebutton.grid(row=2, column=2)
         self.run(onlyFrames=False)
+
+    def close_day(self):
+        self.denom_text.configure(state="normal")
+        self.denom_text.delete("0.0", END)
+        self.denom_text.insert(END, f"{datetime.datetime.now().strftime('%Y-%m-%d %H.%M.%S')}\n{self.adres}В сумке:\n", "center")
+        total = 0
+        for denom, quantity in self.denom_dict.items():
+            self.denom_text.insert(END, f"{denom} руб. - {quantity} шт.\n", "center")
+            total += int(denom) * quantity
+        self.denom_text.insert(END, f"ИТОГО {str(total)} руб.", "center")
+        self.denom_text.configure(state="disabled")
+        nt[0].select(5)
 
     def tree_selection(self, event):
         item = self.tree_data.item(self.tree_data.selection())
@@ -172,10 +187,8 @@ class App(tkm.ThemedTKinterFrame):
     def receipt(self):
         receipt_table = PrettyTable(["Время внесения", "Сумма"], border=False)
         receipt_table.add_row([datetime.datetime.now().strftime("%Y-%m-%d %H.%M.%S"), "2000"])
-        receipt_total = f"АДМ №213445 121096 г.Москва\nул.Кастанаевская, д.24 \nEMAIL: sales@deep2000.ru\n" \
-                        f"{self.receipt_number}\n{self.client}\n{self.account}\n\n{receipt_table}\n\n ИТОГО 2000"
-
-        self.receipt_text.configure(text=receipt_total, font="Courier", justify="center" )
+        receipt_total = f"{self.adres}{self.receipt_number}\n{self.client}\n{self.account}\n\n{receipt_table}\n\n ИТОГО 2000"
+        self.receipt_text.configure(text=receipt_total, font="Courier", justify="center")
         self.make_qr(receipt_total)
         self.print_text(receipt_total)
         self.print_qr("print_qr.png")
